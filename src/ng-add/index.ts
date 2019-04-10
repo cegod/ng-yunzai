ae7b82e806a54d4bbadb0c4a1493c894612da3cf
ferried <harlancui@outlook.com>
2019-04-10 12:01:39 +0800

    feat(module:all): over package

--- src/ng-add/index.ts
+++ src/ng-add/index.ts
@@ -1,100 +1,130 @@
 import {Schema} from './schema';
 import {
-  apply,
-  chain,
-  MergeStrategy,
-  mergeWith,
-  move,
-  Rule,
-  SchematicContext, template,
-  Tree,
-  url,
+    apply,
+    chain,
+    MergeStrategy,
+    mergeWith,
+    move,
+    Rule,
+    SchematicContext,
+    template,
+    Tree,
+    url,
 } from '@angular-devkit/schematics';
-import {NodePackageInstallTask} from '@angular-devkit/schematics/tasks';
 import {getProjectFromWorkspace} from '@angular/cdk/schematics';
 import {getWorkspace} from '@schematics/angular/utility/config';
+import {addPackageJsonDependency, NodeDependency, NodeDependencyType} from "@schematics/angular/utility/dependencies";
+import {NodePackageInstallTask} from "@angular-devkit/schematics/tasks";
 
 export default function (_options: Schema): Rule {
-  return (_tree: Tree, _context: SchematicContext) => {
-    return chain([
-      // 安装依赖
-      addPackageToPackageJson(_tree, _context),
-      // 删除
-      removeOrginalFiles(_options),
-      // 创建
-      addFiles(_tree, _context, _options)
-    ]);
-  };
+    return (_tree: Tree, _context: SchematicContext) => {
+        return chain([
+            // 生成依赖
+            generatePackageToPackageJson(_context),
+            // 生成scripts sonar hooks
+            generateScriptsSonarHooks(_context),
+            // 下载所有包
+            downLoadAllPackages(_context),
+            // 删除
+            removeOrginalFiles(_options),
+            // 创建
+            addFiles(_tree, _context, _options)
+        ]);
+    };
+}
+
+const dependencies: NodeDependency[] = [
+    {type: NodeDependencyType.Default, version: '1.1.20-alpha', name: 'yunzai'},
+    {type: NodeDependencyType.Default, version: '^4.14.123', name: 'lodash'},
+    {type: NodeDependencyType.Default, version: '^4.14.123', name: '@types/lodash'},
+    {type: NodeDependencyType.Default, version: '^7.2.0', name: '@stomp/ng2-stompjs'},
+    {type: NodeDependencyType.Dev, version: '^1.3.1', name: 'husky'},
+    {type: NodeDependencyType.Dev, version: '^2.14.0', name: 'validate-commit-msg'},
+    {type: NodeDependencyType.Dev, version: '^2.0.12', name: 'conventional-changelog-cli'},
+    {type: NodeDependencyType.Dev, version: '^2.4.0', name: 'sonarqube-scanner'},
+
+]
+
+
+export function generatePackageToPackageJson(_context: SchematicContext): Rule {
+    return (host: Tree) => {
+        dependencies.forEach(dependency => {
+            addPackageJsonDependency(host, dependency);
+            _context.logger.log('info', `️Added "${dependency.name}" into ${dependency.type}`);
+        });
+        return host;
+    };
 }
 
 
-export function addPackageToPackageJson(_tree: Tree, _context: SchematicContext): Rule {
-  return (host: Tree) => {
-    const packageList = [
-      'ng-zorro-antd',
-      'yunzai',
-      '@stomp/ng2-stompjs',
-      'sonarqube-scanner',
-      'lodash',
-      '@types/lodash',
-      'conventional-changelog-cli',
-      'validate-commit-msg',
-      'husky'
-    ];
-    if (host.exists('package.json')) {
-      // @ts-ignore
-      const sourceText = host.read('package.json').toString('utf-8');
-      const json = JSON.parse(sourceText);
-      if (!json.dependencies) {
-        json.dependencies = {};
-      }
-      for (let pkg of packageList) {
-        if (!json.dependencies[pkg]) {
-          json.dependencies[pkg] = '';
+export function generateScriptsSonarHooks(_context: SchematicContext): Rule {
+    return (host: Tree) => {
+        if (host.exists('package.json')) {
+            // @ts-ignore
+            const sourceText = host.read('package.json').toString('utf-8');
+            const json = JSON.parse(sourceText);
+            if (!json.scripts) {
+                json.scripts = {}
+            } else {
+                json.scripts.lint = "ng lint";
+                json.scripts.fixcode = "yarn lint --fix";
+                json.scripts.version = "./node_modules/.bin/conventional-changelog -p angular -i CHANGELOG.md -s --pkg package.json";
+                json.scripts.scanner = "yarn fixcode && ./node_modules/.bin/sonar-scanner"
+            }
+            if (!json.husky) {
+                json.husky = {hooks: {}}
+            }
+            json.husky.hooks['pre-commit'] = "yarn lint";
+            json.husky.hooks['commit-msg'] = "validate-commit-msg";
+            host.overwrite('package.json', JSON.stringify(json, null, 2));
         }
-      }
-      host.overwrite('package.json', JSON.stringify(json, null, 2));
-    }
-    _context.addTask(new NodePackageInstallTask());
-    return host;
-  };
+        _context.addTask(new NodePackageInstallTask());
+        return host;
+    };
+}
+
+export function downLoadAllPackages(_context: SchematicContext): Rule {
+    return (host: Tree) => {
+        _context.addTask(new NodePackageInstallTask());
+        return host;
+    };
 }
 
 function removeOrginalFiles(options: Schema) {
-  return (host: Tree) => {
-    const workspace = getWorkspace(host);
-    const project = getProjectFromWorkspace(workspace, options.project);
-    [
-      `${project.sourceRoot}/environments/environment.prod.ts`,
-      `${project.sourceRoot}/environments/environment.ts`,
-      `${project.sourceRoot}/index.html`,
-      `${project.sourceRoot}/app/app.module.ts`,
-      `${project.sourceRoot}/app/app.component.ts`,
-      `${project.sourceRoot}/app/app.component.html`,
-      `${project.sourceRoot}/app/app.component.less`,
-      `${project.sourceRoot}/app/app.component.css`,
-      `${project.sourceRoot}/app/app-routing.module.ts`,
-    ]
-      .filter(p => host.exists(p))
-      .forEach(p => host.delete(p));
-    return host;
-  };
+    return (host: Tree) => {
+        const workspace = getWorkspace(host);
+        const project = getProjectFromWorkspace(workspace, options.project);
+        [
+            `${project.sourceRoot}/environments/environment.prod.ts`,
+            `${project.sourceRoot}/environments/environment.ts`,
+            `${project.sourceRoot}/index.html`,
+            `${project.sourceRoot}/app/app.module.ts`,
+            `${project.sourceRoot}/app/app.component.ts`,
+            `${project.sourceRoot}/app/app.component.html`,
+            `${project.sourceRoot}/app/app.component.less`,
+            `${project.sourceRoot}/app/app.component.css`,
+            `${project.sourceRoot}/app/app-routing.module.ts`,
+        ]
+            .filter(p => host.exists(p))
+            .forEach(p => host.delete(p));
+        return host;
+    };
 }
 
 function addFiles(_tree: Tree, _context: SchematicContext, options: Schema) {
-  const workspace = getWorkspace(_tree);
-  const project = getProjectFromWorkspace(workspace, options.project);
-  return chain([
-      mergeWith(
-        apply(url('./files/src'), [
-          template({
-            ...options,
-          }),
-          move(project.sourceRoot as string),
-        ]),
-        MergeStrategy.Overwrite
-      ),
-    ]
-  );
+    const workspace = getWorkspace(_tree);
+    const project = getProjectFromWorkspace(workspace, options.project);
+    return chain([
+            mergeWith(
+                apply(url('./files/src'), [
+                    template({
+                        ...options,
+                    }),
+                    move(project.sourceRoot as string),
+                ]),
+                MergeStrategy.Overwrite
+            ),
+        ]
+    );
 }
 
